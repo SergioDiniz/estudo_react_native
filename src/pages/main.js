090efa592023b97f306d21556e392a1302bf8c57
Sergio Diniz <sergiodinizsh@gmail.com>
2018-10-21 14:49:22 -0300

    estilização da lista de item

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 
 import api from "../services/api";
 
 export default class Main extends Component {
   // para mudar o titulo default
   static navigationOptions = {
-    title: "JSHunt"
+    title: "Listagem de produtos"
   };
 
   state = {
@@ -25,14 +31,84 @@ export default class Main extends Component {
     console.log(docs);
   };
 
+  renderItem = ({ item }) => {
+    return (
+      <View style={style.productContainer}>
+        <Text style={style.productTitle}>{item.title}</Text>
+        <Text style={style.productDescription}>{item.description}</Text>
+
+        <TouchableOpacity
+          style={style.productButton}
+          onPress={() => {
+            console.log(`Clicou em ${item.title}`);
+          }}
+        >
+          <Text style={style.productButtonTitle}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   render() {
     return (
-      <View>
-        <Text>Página Main </Text>
-        {this.state.docs.map(product => (
-          <Text key={product._id}>{product.title}</Text>
-        ))}
+      <View style={style.container}>
+        <FlatList
+          contentContainerStyle={style.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFAFA"
+  },
+
+  list: {
+    padding: 20
+  },
+
+  productContainer: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+
+  productTitle: {
+    color: "#333",
+    fontWeight: "bold",
+    fontSize: 18
+  },
+
+  productDescription: {
+    marginTop: 5,
+    lineHeight: 24,
+    fontSize: 16,
+    color: "#999"
+  },
+
+  productButton: {
+    height: 45,
+    borderColor: "#DA552F",
+    borderRadius: 5,
+    borderWidth: 1,
+    backgroundColor: "transparent",
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  productButtonTitle: {
+    fontSize: 16,
+    color: "#DA552F",
+    fontWeight: "bold"
+  }
+});
